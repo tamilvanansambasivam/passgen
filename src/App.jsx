61f6935d553b657083e85a835ba8ae14e01ee724
tamilvanan <tamilvanansambasivam@gmail.com>
2024-04-09 08:48:38 +0530

    updated the code style

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 function App() {
   const [password, setPassword] = useState("");
-
+  const [copybtn, setCopybtn] = useState("Copy text");
   function makeUniqueId(length) {
     let result = "";
     const characters =
@@ -18,58 +18,61 @@ function App() {
 
   function genNewPassword() {
     setPassword(makeUniqueId(20));
+    setCopybtn("Copy text");
   }
 
   function copyToClipboard() {
     navigator.clipboard.writeText(password);
+    setCopybtn("Test copied");
   }
 
   return (
-    <div className="m-10 space-y-4">
-      <label
-        htmlFor="helper-text"
-        className="block mb-2  font-medium text-gray-900 dark:text-white text-5xl"
-      >
-        Password Generator{" "}
-      </label>
-      <input
-        type="text"
-        id="myInput"
-        value={password}
-        readOnly
-        aria-describedby="helper-text-explanation"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="(Aq%84d^2PZKIUTHK)W5"
-      />
-      <p
-        id="helper-text-explanation"
-        className="mt-2 text-sm text-gray-500 dark:text-gray-400"
-      >
-        We'll never share your details. Read our{" "}
-        <a
-          href="#"
-          className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-        >
-          Privacy Policy
-        </a>
-        .
-      </p>
+    <div class="flex justify-center items-center h-screen">
+      <div class="bg-slate-100 m-4 sm:p-8 rounded-lg shadow-lg">
+        <div className="m-10 space-y-4">
+          <label
+            htmlFor="helper-text"
+            className="block mb-2 font-medium text-gray-700 text-3xl lg:text-5xl"
+          >
+            Password Generator{" "}
+          </label>
+          <input
+            type="text"
+            id="myInput"
+            value={password}
+            readOnly
+            aria-describedby="helper-text-explanation"
+            className="text-sm rounded-lg  block w-full p-2.5   bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+            placeholder="(Aq%84d^2PZKIUTHK)W5"
+          />
+          <p
+            id="helper-text-explanation"
+            className="mt-2 text-sm text-gray-400"
+          >
+            We'll never share your details. Read our{" "}
+            <a href="#" className="font-medium hover:underline text-blue-500">
+              Privacy Policy
+            </a>
+            .
+          </p>
 
-      <button
-        type="button"
-        className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-        onClick={copyToClipboard}
-      >
-        Copy text
-      </button>
+          <button
+            type="button"
+            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none  focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            onClick={copyToClipboard}
+          >
+            {copybtn}
+          </button>
 
-      <button
-        type="button"
-        className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-        onClick={genNewPassword}
-      >
-        Generate
-      </button>
+          <button
+            type="button"
+            className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none  focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            onClick={genNewPassword}
+          >
+            Generate
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
